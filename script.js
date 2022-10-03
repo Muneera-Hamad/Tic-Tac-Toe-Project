223c02641b0ae994157527663e95b40c00243987
Muneera-Hamad <info.muneera@gmail.com>
2022-10-03 17:10:55 +0300

    working on css

---
+++ script.js
@@ -0,0 +1,13 @@
+const firstbox = document.getElementById("1");
+const firstbox = document.getElementById("2");
+const firstbox = document.getElementById("3");
+const firstbox = document.getElementById("4");
+const firstbox = document.getElementById("5");
+const firstbox = document.getElementById("6");
+const firstbox = document.getElementById("7");
+const firstbox = document.getElementById("8");
+const firstbox = document.getElementById("9");
+
+firstbox.addEventListener('click',function(){
+alert("Hi this suare is clicked")
+})
